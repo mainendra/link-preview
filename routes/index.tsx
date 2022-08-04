8e8bc574e3b15d7f13448cdad2be71a0626f0eec
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-08-04 17:41:20 -0400

    Moved meata to route

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,10 +2,24 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import Preview from "../islands/Preview.tsx";
+import Meta from "../components/Meta.tsx";
+
+const metadata = {
+    documentTitle: 'Link Preview',
+    title: 'Link Preview',
+    description: 'Debug metadata code',
+    og: {
+        url: 'https://m-link-preview.deno.dev',
+        title: 'Link Preview',
+        description: 'Debug metadata code',
+        image: 'https://avatars.githubusercontent.com/u/1897340?v=4',
+    }
+};
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
+        <Meta {...metadata} />
         <Preview />
     </div>
   );
