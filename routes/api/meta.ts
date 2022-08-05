9bd27ace64e4d9c7e5a36b64e3d97f1394574199
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-08-04 21:25:05 -0400

    debug logs

--- routes/api/meta.ts
+++ routes/api/meta.ts
@@ -22,9 +22,9 @@ function parseMetaTags(metaTagElements: Element[]) {
 export const handler: Handlers = {
   async POST(req: Request) {
     try {
-      const { url } = await req.json();
+      const { url, origin } = await req.json();
+      console.log(origin);
       const result = await fetch(decodeURIComponent(url)).then(resp => resp.text());
-      console.log(result);
       const dom = (new DOMParser()).parseFromString(result, 'text/html');
       const documentTitle = dom?.querySelector('title')?.innerHTML;
       const metaTagElements = dom?.getElementsByTagName('meta') ?? [];
